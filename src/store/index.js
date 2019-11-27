f76a5fea8b4335f283a7a6c1f63257ab5be25459
koki imaeda <koki.imaeda@optim.co.jp>
2019-11-28 00:20:06 +0900

    import vuex

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+// プラグインを登録
+Vue.use(Vuex)
+ 
+// ストアを作成
+const store = new Vuex.Store({
+  state: {
+      
+  },
+  getters: {
+    
+  },
+  mutations: {
+    // カウントアップするミューテーションを登録
+  }
+})
+ 
+export default store
